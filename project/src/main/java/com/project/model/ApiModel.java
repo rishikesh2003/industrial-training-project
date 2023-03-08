b54f2a5c1a2c93c23393c7beaef56feab9b15f14
ronny <rkesh2003@gmail.com>
2023-03-08 14:16:08 +0530

    initial commit

---
+++ project/src/main/java/com/project/model/ApiModel.java
@@ -0,0 +1,44 @@
+package com.project.model;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
+@Entity
+public class ApiModel {
+	@Id
+	private int id;
+	private String name;
+	private int quantity;
+	private float price;
+	private String img;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getQuantity() {
+		return quantity;
+	}
+	public void setQuantity(int quantity) {
+		this.quantity = quantity;
+	}
+	public float getPrice() {
+		return price;
+	}
+	public void setPrice(float price) {
+		this.price = price;
+	}
+	public String getImg() {
+		return img;
+	}
+	public void setImg(String img) {
+		this.img = img;
+	}
+	
+}
